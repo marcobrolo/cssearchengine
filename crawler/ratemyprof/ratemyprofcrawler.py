3beeaa722fdd702e22a67363243b6f403414145c
marco_brolo <markgorlui@gmail.com>
2013-03-12 17:00:47 -0700

    included initial python crawler file for ratemyprof

---
+++ crawler/ratemyprof/ratemyprofcrawler.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+
+# url of ratemyprof's SFU compsci: index of all compsci profs
+BASE_URL = "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482"
+
+# parse ratemyprof and download professor's webpage
+# figure out how they wrap professors profile page links
+# figure out how they wrap "next page" links to access
+   #next pages for professors
+
+def main():
+
+
+
+if __name__ "__main__":
+    main()
