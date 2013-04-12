4a8274a1a1c4e01d7df3620220393d2e87911b49
davinche <vinche7@gmail.com>
2013-04-11 21:51:57 -0700

    database loaded with data

---
+++ crawler/SFUScrape/sfuscrape/sfuscrape/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for sfuscrape project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'sfuscrape'
+
+SPIDER_MODULES = ['sfuscrape.spiders']
+NEWSPIDER_MODULE = 'sfuscrape.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'sfuscrape (+http://www.yourdomain.com)'
