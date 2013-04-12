4ff4615583c046e3d2f8c7df1ce492048a472eb2
Masur Ho <mushyho@gmail.com>
2013-04-11 18:05:10 -0700

    models db haystack setup

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 import haystack
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 haystack.autodiscover()
 
@@ -15,7 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	#Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 )
