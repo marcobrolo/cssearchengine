a988460a791a52dac226572fdb4860b17c257c8d
Masur Ho <mushyho@gmail.com>
2013-04-18 00:57:44 -0700

    COMMENTS BRO

--- engine/views.py
+++ engine/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.template import Context, loader, RequestContext
 from django.db import models
-from models import Prof, Course
+from models import Prof, Course, CourseRating
 from haystack.forms import ModelSearchForm, HighlightedSearchForm
 from haystack.query import SearchQuerySet
 from haystack.views import FacetedSearchView
@@ -21,15 +21,17 @@ def results_page(request):
 def professor_profile(request, prof_id):
     # retrieve prof info from database
     professor = Prof.objects.get(pk=prof_id)
-
+    CR = CourseRating.objects.filter(course=prof_id)
     # return object to template so it can acess professor attributes easier
     # probably very insecure and non conventional
-    return render_to_response('professor_profile.html', {'professor': professor}, context_instance=RequestContext(request))
+    return render_to_response('professor_profile.html', {'professor': professor, 'comments': CR}, context_instance=RequestContext(request))
 
 
 def course_profile(request, course_id):
     course = Course.objects.get(pk=course_id)
-    return render_to_response('course_profile.html', {'course': course}, context_instance=RequestContext(request))
+    CR = CourseRating.objects.filter(course=course_id)
+
+    return render_to_response('course_profile.html', {'course': course, 'comments': CR}, context_instance=RequestContext(request))
 
 
 def search_view(request):
