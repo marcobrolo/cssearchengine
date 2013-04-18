ea42904f4e6aa3d114ca022c504ebed25037be62
marco_brolo <markgorlui@gmail.com>
2013-04-18 01:42:56 -0700

    fixed many bugs including why some profs are not scraped to fixing fuzzy strings with first names

--- crawler/tutorial/tutorial/spiders/dmoz_spider.py
+++ crawler/tutorial/tutorial/spiders/dmoz_spider.py
@@ -11,6 +11,39 @@ import re
 import json
 from scrapy.xlib.pydispatch import dispatcher
 
+class SFU_faculty_spider(BaseSpider):
+    name = "sfufaculty"
+    allowed_domains = ["cs.sfu.ca"]
+    start_urls =[
+        "http://www.cs.sfu.ca/people/faculty.html"
+    ]
+
+    def parse(self, response):
+        first_name = ''
+        last_name = ''
+        prof_list = {}
+
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//div[@class="textimage section"]')
+        for site in sites:
+            name_scrap = site.select('div[@class="ruled"]/div[@class="text"]/h3/text()').extract()
+            name_scrap = str(name_scrap)[3:-2]
+            print name_scrap
+            name_scrap = name_scrap.split(",")[0]
+            name_scrap = name_scrap.split(" ")
+            print name_scrap
+            first_name = name_scrap[0]
+            if first_name != '':
+                try:
+                    last_name = name_scrap[2]
+                except:
+                    print name_scrap
+                    last_name = name_scrap[1]
+
+            print first_name, last_name
+
+
+
 class RMPSpider(BaseSpider):
     counter = 1
     course_dict = {}
@@ -19,12 +52,14 @@ class RMPSpider(BaseSpider):
     name = "ratemyprof"
     allowed_domains = ["ratemyprofessors.com"]
     start_urls = [
-        "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482"
+        "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482",
         "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482&pageNo=2",
         "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482&pageNo=3",
         "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482&pageNo=4",
         "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482&pageNo=5",
-        "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482&pageNo=6"
+        "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=1482&pageNo=6",
+        "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=4267",
+        "http://www.ratemyprofessors.com/SelectTeacher.jsp?the_dept=Computer+Science&orderby=TLName&sid=4267&pageNo=2"
     ]
     def index_prof_name_extract_refine(self, name):
         '''
@@ -72,6 +107,7 @@ class RMPSpider(BaseSpider):
         items = []
         for site in sites:
             prof_link = site.select('a/@href').extract()
+            print "we are visiting", site.select('a/text()').extract()
             
             next_page = "http://www.ratemyprofessors.com/" + str(prof_link)[3:-2]            
             if next_page:
@@ -92,11 +128,11 @@ class RMPSpider(BaseSpider):
         path = "//home//draco//Dropbox//SFU//cmpt456//project//cssearchengine//crawler//tutorial//files//"
         first_name =''      # used for djangoitem
         lastname = ''       # used for djangoitem
-        name = ''           # used for filename for saving prof profile page
-        quality = ''        # used for prof rating scrape from rmp site
-        helpfulness = ''    # used for prof rating scrape from rmp site
-        easiness = ''       # used for prof rating scrape from rmp site
-        clarity = ''        # used for prof rating scrape from rmp site
+        name = 'N/A'           # used for filename for saving prof profile page
+        quality = 'N/A'        # used for prof rating scrape from rmp site
+        helpfulness = 'N/A'    # used for prof rating scrape from rmp site
+        easiness = 'N/A'       # used for prof rating scrape from rmp site
+        clarity = 'N/A'        # used for prof rating scrape from rmp site
         
         profList = []       # holds list of prof objects
         class_list = []     # holds list of class objects to store comments and ratings
@@ -110,15 +146,22 @@ class RMPSpider(BaseSpider):
         # grab prof information and save the page
         # grab prof name below
         sites = hxs.select('/html/head/title')
+        f = open("prof_name_errors.txt", 'w')
         for site in sites:
+
             name = str(site.select('text()').extract())
-            print ":::::::::::::::::::::::::::::", name
+            #print ":::::::::::::::::::::::::::::", name, url
             firstname, lastname = self.prof_profile_extract_name(name)
+            # error check
+            if firstname == '' and lastname == '':
+                print name, "HAS ERRORS"
+                f.write(name)
+                f.write("\n")
             name = firstname + "_" + lastname
-            print "WWWWWWWWW", name
+            print "WWWWWWWWW", name, str(self.counter)
         if not os.path.exists(path + name + "//"):
             os.makedirs(path+name+"//")
-
+        f.close()
         # save prof page here    
         #filename = path + name + "//" + name + "_" + prof_profile_page_number
         #open(filename, 'wb').write(response.body)
@@ -126,29 +169,42 @@ class RMPSpider(BaseSpider):
         # get the score card
         # we only need to parse this once
         if flag == False:
+            # get overall quality
+            quality = str(hxs.select('//li[@id="quality"]').extract())
             try:
-                # get overall quality
-                quality = str(hxs.select('//li[@id="quality"]').extract())
                 quality = quality.split("strong>")[1][:-2]
-                #print "Quality: ", quality
-
-                # get helpfulness
-                helpfulness = str(hxs.select('//li[@id="helpfulness"]').extract())
-                helpfulness = helpfulness.split("strong>")[1][:-2]
-                #print "helpfulness: ", helpfulness
-
-                # get clarity
-                clarity = str(hxs.select('//li[@id="clarity"]').extract())
-                clarity = clarity.split("strong>")[1][:-2]
-                #print "Clarity: ", clarity
-
-                # get easiness
-                easiness = str(hxs.select('//li[@id="easiness"]').extract())
-                easiness = easiness.split("strong>")[1][:-2]
-                #print "Easiness: ", easiness
-
             except:
-                print("rank index oor")
+                quality = ''
+                print "quality issues", quality
+            #print "Quality: ", quality
+
+            # get helpfulness
+            helpfulness = str(hxs.select('//li[@id="helpfulness"]').extract())
+            try:
+                helpfulness = helpfulness.split("strong>")[1][:-2]
+            except:
+                helpfulness = ''
+                print "helpfulness issue", helpfulness
+            #print "helpfulness: ", helpfulness
+
+            # get clarity
+            clarity = str(hxs.select('//li[@id="clarity"]').extract())
+            try:
+                clarity = clarity.split("strong>")[1][:-2]
+            except:
+                clarity = ''
+                print "clarity issue", clarity
+            #print "Clarity: ", clarity
+
+            # get easiness
+            easiness = str(hxs.select('//li[@id="easiness"]').extract())
+            try:
+                easiness = easiness.split("strong>")[1][:-2]
+            except:
+                easiness = ''
+                print "easiness issue", easiness
+            #print "Easiness: ", easiness
+
         # we now try to get classes and comments
         # EVEN
         sites = hxs.select('//div[@class="entry even"]')
@@ -185,6 +241,7 @@ class RMPSpider(BaseSpider):
             current_course_dict[course_name] = rating_dict
 
         #ODD
+        sites = hxs.select('//div[@class="entry odd"]')
         for site in sites:
             course_name = ''
             course_easiness = '0'
@@ -225,10 +282,13 @@ class RMPSpider(BaseSpider):
         prof_dict['easiness'] = easiness
         prof_dict['course_rating'] = current_course_dict
         self.prof_list_dict[firstname+lastname]=prof_dict
-        json_file_name = firstname+ "_"+ lastname + prof_profile_page_number + '.json'
+        #json_file_name = firstname+ "_"+ lastname + prof_profile_page_number + '.json'
         json_file_name = "profdata"+str(self.counter)
+        #json_file_name = str(firstname) + "_" + str(lastname) + str(self.counter)
+        #print json_file_name
         self.counter +=1
         json_path = "prof_files/" + json_file_name
+        #print json_path
         with open(json_path, 'w') as outfile:
                 json.dump(prof_dict, outfile, indent=4)
         outfile.close()
--- profload.py
+++ profload.py
@@ -1,6 +1,7 @@
 import json
 import sqlite3 as lite
 from pprint import pprint
+import Levenshtein as lev
 
 file_counter = 1
 con = lite.connect('eg.db')
@@ -9,11 +10,11 @@ prof_list_db_lastname = []
 prof_list_repeated_lastnames = []
 prof_list_db_dict = {}
 prof_not_in_db_list= []
-
+prof_firstname_id_dict ={}
 
 def get_prof_from_db(id):
 	# printing out prof item in db based on the db id
-    cur.execute("SELECT * FROM engine_prof WHERE Id="+str(prof_list_db_dict[prof_name_last]))
+    cur.execute("SELECT * FROM engine_prof WHERE Id="+str(id))
     row = cur.fetchone()
     if row == None:
                 print "ERROR: our prof_list_db_dict id is not in DB"
@@ -26,19 +27,29 @@ with con:
     
     while(True):
     	# fetch prof items from db and add into offline lists for post processing
+    	prof_firstname_id_dict = {}
     	row = cur.fetchone()
     	if row == None:
         	break
         if row[1] not in prof_list_db_lastname:
         	prof_list_db_lastname.append(row[1])
         	prof_list_db_fullname.append((row[2]+row[1]))
-        	prof_list_db_dict[row[1]] = row[0]
+        	prof_firstname_id_dict[row[2]] = row[0]
+        	prof_list_db_dict[row[1]] = prof_firstname_id_dict
         else:
+        	prof_firstname_id_dict[row[2]] = row[0]
+        	for key, value in prof_list_db_dict[row[1]].iteritems():
+        		prof_firstname_id_dict[key] = value
+        	prof_list_db_dict[row[1]] = (prof_firstname_id_dict)
+        	for key, value in prof_list_db_dict[row[1]].iteritems():
+        		print "EEEEEEEEEEE multiple lsat names", row[1], key 
+        		prof_firstname_id_dict[key] = value
         	prof_list_db_fullname.append((row[2]+row[1]))
         	prof_list_repeated_lastnames.append((row[1],row[2]))
         	print "we got same last names ", row[1]
 print prof_list_db_fullname
 while(1):
+	print "file counter", file_counter
 	file_name = "profdata"+ str(file_counter)
 	file_path = "prof_files/" + file_name
 	file_counter += 1
@@ -52,7 +63,22 @@ while(1):
 	fp = open(file_path)
 	profs = json.load(fp)
 	prof_name_last = profs["last_name"]
+	prof_name_first = profs["first_name"]
 	prof_name_full = profs["first_name"]+profs["last_name"]
+
+	#figure out alternative names and same last names
+	if prof_name_last in prof_list_db_lastname:
+		# we got same last name but figure out first name
+		lev_ratio = 0.0
+		for possible_prof in prof_list_db_dict[prof_name_last]:
+			#print "WTF", possible_prof
+			#find same last name dif first name
+			if lev.ratio(possible_prof, prof_name_first) > lev_ratio:
+				print"fixing name", prof_name_full, prof_name_first, possible_prof
+				prof_name_first = possible_prof
+				prof_name_full = prof_name_first+profs["last_name"]
+				print "now", prof_name_full
+
 	if prof_name_last not in prof_list_db_lastname:
 		print prof_name_last, " not in db"
 		prof_not_in_db_list.append(prof_name_full)
@@ -60,20 +86,28 @@ while(1):
 	else:
 		if prof_name_full not in prof_list_db_fullname:
 			prof_not_in_db_list.append(prof_name_full)
-			#print prof_name_full, "has shortened name"
+			print "EEEEEEEEEE", prof_name_full, "different first names"
 			continue
-		else:
+		elif prof_name_full in prof_list_db_fullname:
 			#print prof_name_full , " prof id is", prof_list_db_dict[prof_name_last]
-			cur.execute("SELECT * FROM engine_prof WHERE Id="+str(prof_list_db_dict[prof_name_last]))
+			db_id = ''
+			for key, value in prof_list_db_dict[prof_name_last].iteritems():
+				if key == prof_name_first:
+					db_id = value
+			if db_id == '':
+				print "EEEEEEEEEEEEE db_id not avail maybe matching prof dictionary no good"
+
+			cur.execute("SELECT * FROM engine_prof WHERE Id="+str(db_id))
 			row = cur.fetchone()
 			if row == None:
 				print "ERROR: our prof_list_db_dict id is not in DB"
 				exit()
 			# begin adding in the ratings
-			print "before update ranks", get_prof_from_db(str(prof_list_db_dict[prof_name_last]))
-			cur.execute("UPDATE engine_prof SET helpfulness=?, clarity=?, easiness=? WHERE Id=?", (profs["helpfulness"], profs["clarity"], profs["easiness"], str(prof_list_db_dict[prof_name_last])))        
+			print db_id, prof_name_full
+			#print "before update ranks", get_prof_from_db(str(db_id))
+			cur.execute("UPDATE engine_prof SET helpfulness=?, clarity=?, easiness=? WHERE Id=?", (profs["helpfulness"], profs["clarity"], profs["easiness"], str(db_id)))        
     		con.commit()
-    		print "after update ranks ", get_prof_from_db(str(prof_list_db_dict[prof_name_last]))
+    		#print "after update ranks ", get_prof_from_db(db_id)
 			# we can start adding in the values of scrapped data
 		#print prof_name, " in db"
 		continue
